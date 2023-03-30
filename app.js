6464dc1729e9d16b05acd0ea02170120da6e78c9
ersinev <ersinevci@hotmail.com>
2023-03-30 21:47:02 +0200

    updated

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-// const express = require("express")
-// const cors = require("cors");
-// const app =express()
-// app.use(cors())
-// const mongoose = require("mongoose");
-// const userRouter = require("./Users/UserController");
+const express = require("express")
+const cors = require("cors");
+const app =express()
+app.use(cors())
+const mongoose = require("mongoose");
+const userRouter = require("./Users/UserController");
 
 
 
