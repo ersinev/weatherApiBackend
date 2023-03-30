4559d62ac144d60c25cc5dd6ea6518c96b65b75f
ersinev <ersinevci@hotmail.com>
2023-03-30 21:47:24 +0200

    updated

--- app.js
+++ app.js
@@ -7,22 +7,22 @@ const userRouter = require("./Users/UserController");
 
 
 
-// app.use(express.json());
+app.use(express.json());
 
-// const uri = 'mongodb://localhost/weatherapi'; // replace with your MongoDB URI
+const uri = 'mongodb://localhost/weatherapi'; // replace with your MongoDB URI
 
-// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => {
-//     console.log('Connected to MongoDB');
-//     // Your code here
-//   })
-//   .catch((error) => {
-//     console.error('Error connecting to MongoDB', error);
-//   });
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    // Your code here
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB', error);
+  });
 
-// app.listen(5000,()=>{
-//     console.log("server is running on 5000")
-// })
+app.listen(5000,()=>{
+    console.log("server is running on 5000")
+})
 
 
 app.use("/signup", userRouter)
