3391e9a352fe445809779bfa37a9bd52e62b9104
ersinev <ersinevci@hotmail.com>
2023-04-01 18:23:00 +0200

    updated

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const userRouter = require("./Users/UserController");
 
 app.use(express.json());
 
-const uri = 'mongodb://localhost/weatherapi'; 
+const uri = 'mongodb://localhost/weatherapi'; // MongoDB URI
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
